ff713d249c9bf324989ad1422f8cadffad4a6828
Peter Koscelansky <koscelansky@live.com>
2022-12-05 12:33:52 +0100

    Update main

--- AoC-2022/src/main.rs
+++ AoC-2022/src/main.rs
@@ -2,16 +2,18 @@ mod days {
     pub mod day1;
     pub mod day2;
     pub mod day3;
+    pub mod day4;
 }
 
 fn main() {
-    let day = 3;
+    let day = 4;
     let input = "input.txt";
 
     match day {
         1 => days::day1::day1(input),
         2 => days::day2::day2(input),
         3 => days::day3::day3(input),
+        4 => days::day4::day4(input),
         _ => std::process::exit(1)
     }
 }
